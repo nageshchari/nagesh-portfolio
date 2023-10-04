f8fa70d1dad553f2c6615b62752061e7c8c5ebf3
nageshchari <nageshchari75@gmail.com>
2023-10-04 15:06:57 +0530

    nav bar css

--- src/Component/NavBar.js
+++ src/Component/NavBar.js
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useState, useEffect } from 'react';
-import { Navbar, Container} from "react-bootstrap"
+import { Navbar, Container, Nav} from "react-bootstrap"
 import logo from '../assets/img/logo.svg';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
@@ -12,7 +12,7 @@ import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () =>{
     const [activeLink, setActiveLink] = useState('home');
-    const [scolled, seScrolled] = useState('false');
+    const [scrolled, seScrolled] = useState('false');
 
     useEffect(() => {
         const onScrol = () => {
