76ee21c00476652224028762e8cac4e0e8108e65
nageshchari <nageshchari75@gmail.com>
2023-10-08 13:37:49 +0530

    project

--- src/Component/Projects.js
+++ src/Component/Projects.js
@@ -1,5 +1,81 @@
+import { Container, Row } from "react-bootstrap";
+
 export const Projects = () => {
+
+    const projects = [
+        {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg1,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg2,
+          },
+          {
+            title: "Business Startup",
+            description: "Design & Development",
+            imgUrl: projImg3,
+          },
+    ];
+
     return (
-        
+        <section className="project" id='project'>
+            <Container>
+                <Row>
+                    <Col>
+                    <h2>Projects</h2>
+                    <p>Nagesh Chari</p>
+                    <Tab.Container className="projects-tabs" defaultActiveKey="first">
+                    <Nav variant="pills" defaultActiveKey="/home">
+                        <Nav.Item>
+                            <Nav.Link eventKey="first">Tab one</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="second">Tab Two</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link eventKey="third" >
+                            Tab Three
+                            </Nav.Link>
+                        </Nav.Item>
+                        </Nav>
+                        <Tab.Content>
+                            <Tab.Pane eventKey="first">
+                                <Row>
+                                    {
+                                        projects.map((project, index) => {
+                                            return (
+                                                <p>{project.title}</p>
+                                            )
+                                        })
+                                    }
+                                </Row>
+
+                            </Tab.Pane>
+                            <Tab.Pane eventKey="second">Loren Ipsum</Tab.Pane>
+                            <Tab.Pane eventKey="third">Loren Ipsum</Tab.Pane>
+                        </Tab.Content>
+                        </Tab.Container>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
     )
 }
